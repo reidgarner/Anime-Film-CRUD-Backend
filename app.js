e705aa35b49b5945367442a4f0a9cb1d11fbe18c
reidgarner <reidbgarner@gmail.com>
2018-12-06 11:00:10 -0700

    working movie route

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = process.env.PORT || 3004
+const movies = require ('./routes/movies')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use('/movies', movies)
 
 app.listen(port)
