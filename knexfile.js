7f15915e6109068bf566236a6a9223c73f4de158
reidgarner <reidbgarner@gmail.com>
2018-12-06 11:52:47 -0700

    seed running

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/movies'
+  }, 
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+}
